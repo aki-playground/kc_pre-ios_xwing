2b55e476973d07b1e68bef6f6a304adbd0f7d43e
Akixe <akixe.otegi@gmail.com>
2016-02-28 07:45:41 +0100

    Initial Commit

---
+++ Xwing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xwing
+//
+//  Created by Akixe on 28/2/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xwing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Xwing
+//
+//  Created by Akixe on 28/2/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
